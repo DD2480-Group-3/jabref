c8ec84ef1ca791e15033b9792da01479eb35a3e2
Linus Wallin <73783914+LinusWallin@users.noreply.github.com>
2024-03-02 17:24:25 +0100

    Adds feature CleanupJob for removing non-existent files (#2)
    * feat: RemoveLinksToNotExistentFiles.java
    
    * feat: added option to cleanup entries
    
    * feat: improved cleanup entry dialogue
    
    * Test: implements test for linked files which exist
    
    Implementation of a test which calls the cleanup functionality of
    RemoveLinksToNotExistentFiles with a entry that is linked to an existing
    file. The test makes sure that no changes are made since the linked
    file exists.
    
    * Refactor: refactored test to match the standard of the project
    
    Removed the JavaDoc comment from the test, since other tests in the
    project don't have JavaDoc:s. Also removed test from the name of the
    test function, as it seems that the standard way of naming tests
    is to not have test in the testfunctions name.
    
    * Refactor: removed system.out.print()
    
    Removed an print from the tests.
    
    * fix: non english character deleted
    
    * Docs: Added information about changes
    
    Added information about the changes made for solving issue#10929
    to the CHANGES.md file.
    
    ---------
    
    Co-authored-by: burcukilic <94201593+burcukilic@users.noreply.github.com>

---
+++ src/main/java/org/jabref/logic/cleanup/RemoveLinksToNotExistentFiles.java
@@ -0,0 +1,60 @@
+package org.jabref.logic.cleanup;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.jabref.logic.externalfiles.LinkedFileHandler;
+import org.jabref.model.FieldChange;
+import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.LinkedFile;
+import org.jabref.model.util.OptionalUtil;
+import org.jabref.preferences.FilePreferences;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RemoveLinksToNotExistentFiles implements CleanupJob {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemoveLinksToNotExistentFiles.class);
+
+    private final BibDatabaseContext databaseContext;
+    private final FilePreferences filePreferences;
+
+    public RemoveLinksToNotExistentFiles(BibDatabaseContext databaseContext, FilePreferences filePreferences) {
+        this.databaseContext = Objects.requireNonNull(databaseContext);
+        this.filePreferences = Objects.requireNonNull(filePreferences);
+    }
+
+    @Override
+    public List<FieldChange> cleanup(BibEntry entry) {
+        List<LinkedFile> files = entry.getFiles();
+        List<LinkedFile> files2 = new ArrayList<LinkedFile>();
+        boolean changed = false;
+        for (LinkedFile file : files) {
+            LinkedFileHandler fileHandler = new LinkedFileHandler(file, entry, databaseContext, filePreferences);
+            
+            if (file.isOnlineLink() == false) {
+                
+                Optional<Path> oldFile = file.findIn(databaseContext, filePreferences);
+                
+                if (oldFile.isEmpty()) {
+                    changed = true;
+                } else {
+                    files2.add(file);
+                }
+            }
+        }
+
+        if (changed) {
+            Optional<FieldChange> changes = entry.setFiles(files2);
+            return OptionalUtil.toList(changes);
+        }
+
+        return Collections.emptyList();
+    }
+}
---
+++ src/test/java/org/jabref/logic/cleanup/RemoveLinksToNotExistentFilesTest.java
@@ -0,0 +1,83 @@
+package org.jabref.logic.cleanup;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.jabref.logic.bibtex.FileFieldWriter;
+import org.jabref.model.FieldChange;
+import org.jabref.model.database.BibDatabase;
+import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.LinkedFile;
+import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.metadata.MetaData;
+import org.jabref.preferences.FilePreferences;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import org.jabref.logic.cleanup.RemoveLinksToNotExistentFiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RemoveLinksToNotExistentFilesTest {
+    private Path defaultFileFolder;
+    private Path fileBefore;
+    private BibEntry entry;
+    private FilePreferences filePreferences;
+    private BibDatabaseContext databaseContext;
+    private RemoveLinksToNotExistentFiles removeLinks;
+
+    @BeforeEach
+    void setUp(@TempDir Path bibFolder) throws IOException {
+        // The folder where the files should be moved to
+        defaultFileFolder = bibFolder.resolve("pdf");
+        Files.createDirectory(defaultFileFolder);
+
+        // The folder where the files are located originally
+        Path fileFolder = bibFolder.resolve("files");
+        Files.createDirectory(fileFolder);
+        fileBefore = fileFolder.resolve("test.pdf");
+        Files.createFile(fileBefore);
+
+        MetaData metaData = new MetaData();
+        metaData.setDefaultFileDirectory(defaultFileFolder.toAbsolutePath().toString());
+        databaseContext = new BibDatabaseContext(new BibDatabase(), metaData);
+        Files.createFile(bibFolder.resolve("test.bib"));
+        databaseContext.setDatabasePath(bibFolder.resolve("test.bib"));
+
+        entry = new BibEntry();
+        entry.setCitationKey("Toot");
+        entry.setField(StandardField.TITLE, "test title");
+        entry.setField(StandardField.YEAR, "1989");
+        LinkedFile fileField = new LinkedFile("", fileBefore.toAbsolutePath(), "");
+        entry.setField(StandardField.FILE, FileFieldWriter.getStringRepresentation(fileField));
+
+        filePreferences = mock(FilePreferences.class);
+        when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(false); 
+        removeLinks = new RemoveLinksToNotExistentFiles(databaseContext, filePreferences);
+    }
+
+    @Test
+    void deleteLinkedFile() {
+        fileBefore.toFile().delete();
+        List<FieldChange> changes = removeLinks.cleanup(entry);
+        assertFalse(changes.isEmpty());
+    }
+
+    @Test
+    void keepLinksToExistingFiles() {
+        List<FieldChange> changes = removeLinks.cleanup(entry);
+        assertTrue(changes.isEmpty());
+    }
+}
